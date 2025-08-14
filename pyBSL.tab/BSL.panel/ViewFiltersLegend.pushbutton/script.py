7c6e6ab07486d4d8414214bd9665b0cc37cf08df
AcOscar <holger.rasch@gamil.com>
2025-08-14 16:11:11 +0200

    more stable, inckl walltype and wall

--- pyBSL.tab/BSL.panel/ViewFiltersLegend.pushbutton/script.py
+++ pyBSL.tab/BSL.panel/ViewFiltersLegend.pushbutton/script.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
 from pyrevit import forms
-from Autodesk.Revit.DB import *
-
+from Autodesk.Revit.DB import ViewType, FilteredElementCollector, View, TextNoteType, BuiltInParameter, ElementTypeGroup, ElementId, SelectionFilterElement,WallType
+from Autodesk.Revit.DB import Transaction, BuiltInCategory, ViewDuplicateOption, ParameterFilterElement, XYZ ,FilledRegionType, Line, OverrideGraphicSettings, TextNote, FilledRegion, CurveLoop
+from System import Enum
+from System.Collections.Generic import List
 
 __title__ = 'ViewFilterLegend'
 __doc__ = 'Create a legend to all wall type filters from a view'
@@ -9,16 +11,34 @@ __doc__ = 'Create a legend to all wall type filters from a view'
 doc = __revit__.ActiveUIDocument.Document
 uidoc = __revit__.ActiveUIDocument
 
+# P4  region_width_mm  P3
+# +----------------------+
+# ¦                      ¦ region_height_mm    "Arial_normal_schwarz_1.5mm"                  "Arial_normal_schwarz_1.5mm"
+# ¦                      ¦                      text_distance_y_mm
+# +----------------------+ text_distance_x_mm   ¦             text2_distance_mm = 4000       ¦
+# P1                    P2 
 
-region_width_mm = 200
-region_height_mm = 50
-region_spacing_mm = 10
-text_type_name = "3.5mm Arial"
+region_width_mm = 390
+region_height_mm = 200
+region_spacing_mm = 200
+text_distance_x_mm = 1100
+text_distance_y_mm = 175
+text2_distance_mm = 4000
+text_type_name = "Arial_normal_schwarz_1.5mm"
 
+unsupported = set([
+    ViewType.ProjectBrowser,
+    ViewType.DrawingSheet,
+    ViewType.Schedule,
+    ViewType.Report,
+    ViewType.SystemBrowser,
+    ViewType.Internal,
+    ViewType.Undefined,
+    ViewType.DraftingView  
+])
 
 def mm_to_feet(mm):
     return float(mm) / 304.8
-
 
 def get_textnotetype(doc, preferred_name="3.5mm Arial"):
     # Collect all TextNoteTypes
@@ -43,62 +63,132 @@ def get_textnotetype(doc, preferred_name="3.5mm Arial"):
     # 3) last fallback: first available
     return types[0]
 
+def get_wall_type_names_hit_by_filter(doc, view, filt, max_names=None):
+    """
+    Returns sorted, unique WallType names that are actually affected by the filter filt in the view view
+    """
+    names = set()
+    try:
+        # Case A: parameter filter
+        if isinstance(filt, ParameterFilterElement):
+            ef = filt.GetElementFilter()  # Autodesk.Revit.DB.ElementFilter
+            col = (FilteredElementCollector(doc, view.Id)
+                   .OfCategory(BuiltInCategory.OST_Walls)
+                   .WhereElementIsNotElementType()
+                   .WherePasses(ef))
+            for w in col:
+                try:
+                    wt = doc.GetElement(w.GetTypeId())
+                    wt_name =wt.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString()
+                    if isinstance(wt, WallType) and wt_name:
+                        names.add(wt_name)
+                except:
+                    pass
+
+        # Case B: SelectionFilterElement
+        elif isinstance(filt, SelectionFilterElement):
+            vis_walls = (FilteredElementCollector(doc, view.Id)
+                         .OfCategory(BuiltInCategory.OST_Walls)
+                         .WhereElementIsNotElementType()
+                         .ToElementIds())
+            sel_ids = set(eid.IntegerValue for eid in filt.GetElementIds())
+            for eid in vis_walls:
+                if eid.IntegerValue in sel_ids:
+                    try:
+                        w = doc.GetElement(eid)
+                        wt = doc.GetElement(w.GetTypeId())
+                        if isinstance(wt, WallType) and wt.Name:
+                            names.add(wt.Name)
+                    except:
+                        pass
+
+        # Fallback: if another filter element nevertheless offers GetElementFilter()
+        else:
+            getef = getattr(filt, "GetElementFilter", None)
+            if getef:
+                ef = getef()
+                col = (FilteredElementCollector(doc, view.Id)
+                       .OfCategory(BuiltInCategory.OST_Walls)
+                       .WhereElementIsNotElementType()
+                       .WherePasses(ef))
+                for w in col:
+                    try:
+                        wt = doc.GetElement(w.GetTypeId())
+                        if isinstance(wt, WallType) and wt.Name:
+                            names.add(wt.Name)
+                    except:
+                        pass
+    except:
+        pass
+
+    res = sorted(names, key=lambda s: s.lower())
+    if max_names and len(res) > max_names:
+        return res[:max_names] 
+    return res
 
 # ============================
 # GET ALL VIEWS WITH FILTERS
 # ============================
-all_views = FilteredElementCollector(doc).OfClass(View).ToElements()
-views_with_filters = []
-for v in all_views:
+
+safe_views = []
+for v in FilteredElementCollector(doc).OfClass(View).WhereElementIsNotElementType():
     try:
-        if not v.IsTemplate and v.GetFilters():
+        if v.IsTemplate:
+            continue
+        if v.ViewType in unsupported:
+            continue
+        # keine GetFilters() hier!
+        safe_views.append(v)
+    except:
+        continue
+
+if not safe_views:
+    forms.alert("No eligible views found.", exitscript=True)
+
+# schlanke Items: Label -> ElementId
+items = []
+id_by_label = {}
+for v in safe_views:
+    if v.GetFilters():
+
+        label = u"{name}  [{vt}]  (Id:{id})".format(
+            name=v.Name,
+            vt=Enum.GetName(ViewType, v.ViewType),
+            id=v.Id.IntegerValue
+        )
+        items.append(label)
+        id_by_label[label] = v.Id
+
+selected_labels = forms.SelectFromList.show(
+    items,
+    multiselect=True,
+    title="Select Views (filters will be checked after selection)"
+)
+
+if not selected_labels:
+    forms.alert("No views were selected. Canceling...", exitscript=True)
+
+selected_views = [doc.GetElement(id_by_label[lbl]) for lbl in selected_labels]
+
+views_with_filters = []
+for v in selected_views:
+    try:
+        flt_ids = v.GetFilters()  # jetzt sicher(er)
+        if flt_ids and len(flt_ids) > 0:
             views_with_filters.append(v)
     except:
         continue
 
 if not views_with_filters:
-    forms.alert("There are no filtered views in the project.", exitscript=True)
-
-selected_views = forms.SelectFromList.show(
-    views_with_filters,
-    multiselect=True,
-    name_attr="Name",
-    title="Select View Name"
-)
-
-if not selected_views:
-    forms.alert("No views were selected. Canceling...", exitscript=True)
-
-# ============================
-# SELECT TEXT STYLE
-# ============================
-# text_types = FilteredElementCollector(doc).OfClass(TextNoteType).WhereElementIsElementType().ToElements()
-
-# class TextTypeWrapper(object):
-#     def __init__(self, text_type):
-#         self.text_type = text_type
-#         self.Name = text_type.get_Parameter(BuiltInParameter.ALL_MODEL_TYPE_NAME).AsString()
-
-# text_wrappers = [TextTypeWrapper(tt) for tt in text_types]
-
-# selected_wrapper = forms.SelectFromList.show(
-#     text_wrappers,
-#     name_attr="Name",
-#     title="Select Text Type"
-# )
-
-# if not selected_wrapper:
-#     forms.alert("No text style was selected. Canceling...", exitscript=True)
-
-# text_type = selected_wrapper.text_type
-
+    forms.alert("Selected views have no filters.", exitscript=True)
 
 text_type = get_textnotetype(doc, text_type_name)
 
 # ============================
 # SELECT VIEW LEGEND 
 # ============================
-legend_views = [v for v in all_views if v.ViewType == ViewType.Legend and not v.IsTemplate]
+#legend_views = [v for v in all_views if v.ViewType == ViewType.Legend and not v.IsTemplate]
+legend_views = [v for v in safe_views if v.ViewType == ViewType.Legend and not v.IsTemplate]
 
 if not legend_views:
     forms.alert("You must have at least one legend view in the project.", exitscript=True)
@@ -152,27 +242,6 @@ for view in selected_views:
     print("New legend'{}': ".format(name))
     new_legend.Name = name
 
-    # List visible walls
-    walls_collector = FilteredElementCollector(doc, view.Id).OfCategory(BuiltInCategory.OST_Walls).WhereElementIsNotElementType().ToElements()
-    wall_names = []
-    for w in walls_collector:
-        try:
-            name = w.Name
-            if name and name.strip():
-                wall_names.append(name)
-
-        except:
-            continue
-
-    wall_names = sorted(set(wall_names))
-
-    print("Walls visible in the view'{}':".format(view.Name))
-    if wall_names:
-        for name in wall_names:
-            print(" - {}".format(name))
-    else:
-        print(" - (no visible walls)")
-        continue
     # Here you can add logic to create FilledRegions with colors from the filter if you want.
     x_origin = mm_to_feet(20)
     y_origin = mm_to_feet(20)
@@ -196,13 +265,6 @@ for view in selected_views:
 
         if not filt or not isinstance(filt, ParameterFilterElement):
             continue  # not a parameter filter
-
-        # Make sure that walls are affected by the filter.
-        categories = filt.GetCategories()
-
-        if wall_cat_id not in categories:
-            continue
-        print(" - {}".format(filt.Name))
 
         # rectangle position
         p1 = XYZ(x_origin, y_origin - y_offset, 0)
@@ -238,11 +300,15 @@ for view in selected_views:
         new_legend.SetElementOverrides(region.Id, g_override)
 
         # Textnote next to the area
-        text_point = XYZ(p2.X + mm_to_feet(10), (p2.Y + p3.Y) / 2, 0)
-        label_text = filt.Name
-        TextNote.Create(doc, new_legend.Id, text_point, label_text, text_type.Id)
+        text_point = XYZ(p2.X + mm_to_feet(text_distance_x_mm), p2.Y + mm_to_feet(text_distance_y_mm), 0)
+
+        type_names = get_wall_type_names_hit_by_filter(doc, view, filt)
+        first_name = type_names[0] if type_names else u"(no wall type)"
+
+        TextNote.Create(doc, new_legend.Id, text_point, first_name, text_type.Id)
+        
+        text_point2 = XYZ(text_point.X + mm_to_feet(text2_distance_mm), text_point.Y , 0)
+        TextNote.Create(doc, new_legend.Id, text_point2, filt.Name, text_type.Id)
 
         y_offset += region_height + region_spacing
 t.Commit()
-
-#forms.alert(" Legends generated and lists of elements displayed in console.")
