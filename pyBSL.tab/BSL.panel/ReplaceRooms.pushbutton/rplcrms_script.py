d6ae73c2fe936d3cce62680fa1bc75149a712871
BslOscar <holger.rasch@gmail.com>
2025-12-02 17:35:46 +0100

    cleanup

--- pyBSL.tab/BSL.panel/ReplaceRooms.pushbutton/rplcrms_script.py
+++ pyBSL.tab/BSL.panel/ReplaceRooms.pushbutton/rplcrms_script.py
@@ -1,16 +1,12 @@
 # -*- coding: utf-8 -*-
 
-#pylint: disable=invalid-name,import-error,superfluous-parens
-from collections import Counter
-from System.Collections.Generic import List
-from pyrevit import revit, DB
+from pyrevit import revit
 from pyrevit import script
-from Autodesk.Revit.DB import BuiltInCategory
-from Autodesk.Revit.DB import FilteredElementCollector
-from Autodesk.Revit.DB import ElementMulticategoryFilter
-from Autodesk.Revit.DB import BuiltInParameter
-from Autodesk.Revit.DB import TransactionGroup, Transaction
+from Autodesk.Revit.DB import TransactionGroup
 from Autodesk.Revit.DB import XYZ, ElementTransformUtils
+from rpw import db, doc, uidoc
+from Autodesk.Revit.DB import FilteredElementCollector as Fec
+from Autodesk.Revit.DB import BuiltInCategory as Bic
 
         
 __doc__ = 'Replace '\
@@ -19,74 +15,62 @@ __doc__ = 'Replace '\
 
 output = script.get_output()
 
-
-
 def main():
 
     idx = 0
 
-    AllElements = []
-
-    cat_list = [BuiltInCategory.OST_Rooms]
-
-    typed_list = List[BuiltInCategory](cat_list)
-
-    filterCategories = ElementMulticategoryFilter(typed_list)
-
-
-    AllElements =   FilteredElementCollector(revit.doc)\
-                .WherePasses(filterCategories)\
-                .WhereElementIsNotElementType()
-                        
-    EleNums =   AllElements.ToElementIds().Count         
-
-    print (EleNums)
-
-    #try:
     transGroup = TransactionGroup(revit.doc, "pyScript replace Rooms")
     transGroup.Start()
 
-    for room in AllElements:
+    selection = [doc.GetElement(elId) for elId in uidoc.Selection.GetElementIds() if doc.GetElement(elId).Category.BuiltInCategory == Bic.OST_Rooms]
+
+    """if was a selection, use that, otherwise get all rooms"""
+    if selection:
+        rooms = selection
+    else:
+        rooms = list(Fec(doc).OfCategory(Bic.OST_Rooms).WhereElementIsNotElementType().ToElements())
+    
+    """check if we have rooms and the necessary parameters"""
+    if not rooms:
+        print("No rooms found.")
+        return
+
+    EleNums =   rooms.Count         
+
+    print (EleNums)
+
+    for room in rooms:
         print (output.linkify(room.Id))
         rmPT = room.Location
         roomPoint = rmPT.Point
-        level = room.get_Parameter(BuiltInParameter.ROOM_UPPER_LEVEL)
+        level = room.get_Parameter(Bic.ROOM_UPPER_LEVEL)
         pinned = room.Pinned 
         SourceElement = room.Document.GetElement(level.AsElementId())
         
         offset = room.LimitOffset
         
-        trans1 = Transaction(revit.doc, "Unplace Room")
-        trans1.Start()
-        
-        room.Pinned = False
-        room.Unplace()
+        with db.Transaction("Unplace Room"):
+            if pinned:
+                room.Pinned = False
+            room.Unplace()
 
-        trans1.Commit()
+        with db.Transaction("New Room"):
 
-        trans2 = Transaction(revit.doc, "New Room")
-        trans2.Start()
+            topo = revit.doc.get_PlanTopology(SourceElement)
+            circuits = topo.Circuits
 
-        #level = cboLevel.SelectedValue
-        topo = revit.doc.get_PlanTopology(SourceElement)
-        circuits = topo.Circuits
-
-        for circuit in circuits:
-            #print "new room"
-        
-            if not circuit.IsRoomLocated:
-                #print "place"
-                #print room.Location
-                newRoom = revit.doc.Create.NewRoom(room, circuit)
-                newRoom.get_Parameter(BuiltInParameter.ROOM_UPPER_LEVEL).Set(SourceElement.Id)
-                newRoom.LimitOffset = offset
-                move = XYZ(roomPoint.X - rmPT.Point.X, roomPoint.Y - rmPT.Point.Y, rmPT.Point.Z)
-                ElementTransformUtils.MoveElement(revit.doc, newRoom.Id, move)
-                newRoom.Pinned = pinned
-                break
+            for circuit in circuits:
             
-        trans2.Commit()
-        
+                if not circuit.IsRoomLocated:
+                    newRoom = revit.doc.Create.NewRoom(room, circuit)
+                    newRoom.get_Parameter(Bic.ROOM_UPPER_LEVEL).Set(SourceElement.Id)
+                    newRoom.LimitOffset = offset
+                    move = XYZ(roomPoint.X - rmPT.Point.X, roomPoint.Y - rmPT.Point.Y, rmPT.Point.Z)
+                    ElementTransformUtils.MoveElement(revit.doc, newRoom.Id, move)
+                    if pinned:
+                        newRoom.Pinned = pinned
+                    break
+                
         output.update_progress(idx, EleNums)
         idx+=1   
 
@@ -94,9 +78,7 @@ def main():
 
     output.reset_progress()
 
-    #happy to be here
     print('Done')
-
 
 if __name__ == "__main__":
     """Run main and catch exceptions to print them in the output window."""
